43914d57bdaa453cdfca3b73409d9d915ef5cad1
Hanafi <hanafi.o95@gmail.com>
2021-09-23 18:13:03 +0200

    Update dependencies

--- package.js
+++ package.js
@@ -1,23 +1,22 @@
 Package.describe({
-  name: '4fox4:mongo-collection-instances',
-  summary: 'Access your Mongo instances',
-  version: '0.3.6',
-  git: 'https://github.com/4fox4/mongo-collection-instances.git'
+  name: "4fox4:mongo-collection-instances",
+  summary: "Access your Mongo instances",
+  version: "0.3.7",
+  git: "https://github.com/4fox4/mongo-collection-instances.git",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use([
-    'mongo',
-    'lai:collection-extensions@0.2.1_1']);
-  api.addFiles('mongo-instances.js');
+Package.onUse(function (api) {
+  api.versionsFrom(["1.0", "2.3"]);
+  api.use(["mongo", "planable:collection-extensions"]);
+  api.addFiles("mongo-instances.js");
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use([
-    'tinytest',
-    'accounts-base',
-    'mongo',
-    '4fox4:mongo-collection-instances']);
-  api.addFiles('mongo-instances-tests.js');
+    "tinytest",
+    "accounts-base",
+    "mongo",
+    "4fox4:mongo-collection-instances",
+  ]);
+  api.addFiles("mongo-instances-tests.js");
 });
